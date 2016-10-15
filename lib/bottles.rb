755581ed3fd98efa7682857562f3fe686ced9b4a
Jan Filipowski <jachuf@gmail.com>
2016-10-15 21:34:46 +0200

    Refactoring to confirm Sandy Metz' rules

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,33 +1,45 @@
 class Bottles
   TEMPLATE = "%{more_bottles} of beer on the wall, %{more_bottles} of beer.\nTake %{taken} down and pass it around, %{less_bottles} of beer on the wall.\n"
-  PLURAL_BOTTLE = "%d bottles"
+  PLURAL_BOTTLE = "%s bottles"
   SINGULAR_BOTTLE = "1 bottle"
-  ZERO_BOTTLE = "no more bottles"
 
   def verse(number)
-    TEMPLATE % {
-      more_bottles: bottle_form(number),
-      less_bottles: bottle_form(number - 1),
-      taken: taken_form(number)
-    }
+    more_bottles_number = human_number(number)
+    less_bottles_number = human_number(number - 1)
+
+    use_template(more_bottles_number, less_bottles_number)
   end
 
   private
+  def use_template(more_bottles_number, less_bottles_number)
+    TEMPLATE % {
+      more_bottles: bottle_form(more_bottles_number),
+      less_bottles: bottle_form(less_bottles_number),
+      taken: taken_form(more_bottles_number)
+    }
+  end
+
   def bottle_form(number)
-    if number > 1
+    if number != "1"
       PLURAL_BOTTLE % [number]
-    elsif number == 1
-      SINGULAR_BOTTLE
     else
-      ZERO_BOTTLE
+      SINGULAR_BOTTLE
     end
   end
 
   def taken_form(number)
-    if number > 1
+    if number != "1"
       "one"
     else
       "it"
+    end
+  end
+
+  def human_number(number)
+    if number == 0
+      "no more"
+    else
+      "#{number}"
     end
   end
 end
