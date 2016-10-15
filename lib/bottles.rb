9ee60e8896433d5edb79fde23e7ee74294f00f10
Jan Filipowski <jachuf@gmail.com>
2016-10-15 20:57:22 +0200

    First test passing

---
+++ lib/bottles.rb
@@ -0,0 +1,7 @@
+class Bottles
+  VERSE = "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+
+  def verse(number)
+    VERSE
+  end
+end
