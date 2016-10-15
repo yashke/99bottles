85727cea48639a45546a6e062452f00d4d05335d
Jan Filipowski <jachuf@gmail.com>
2016-10-15 21:19:13 +0200

    Third test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,10 +1,21 @@
 class Bottles
-  TEMPLATE = "%{number} bottles of beer on the wall, %{number} bottles of beer.\nTake one down and pass it around, %{lower_number} bottles of beer on the wall.\n"
+  TEMPLATE = "%{more_bottles} of beer on the wall, %{more_bottles} of beer.\nTake one down and pass it around, %{less_bottles} of beer on the wall.\n"
+  PLURAL_BOTTLE = "%d bottles"
+  SINGULAR_BOTTLE = "1 bottle"
 
   def verse(number)
     TEMPLATE % {
-      number: number,
-      lower_number: number - 1
+      more_bottles: bottle_form(number),
+      less_bottles: bottle_form(number - 1)
     }
+  end
+
+  private
+  def bottle_form(number)
+    if number > 1
+      PLURAL_BOTTLE % [number]
+    else
+      SINGULAR_BOTTLE
+    end
   end
 end
