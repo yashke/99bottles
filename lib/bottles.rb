a06c270ecedc1910fb98ee0f4d0bdf84a33a69b6
Jan Filipowski <jachuf@gmail.com>
2016-10-15 21:43:11 +0200

    5th test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,22 +1,32 @@
 class Bottles
   TEMPLATE = "%{more_bottles} of beer on the wall, %{more_bottles} of beer.\nTake %{taken} down and pass it around, %{less_bottles} of beer on the wall.\n"
+  TEMPLATE_EMPTY = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   PLURAL_BOTTLE = "%s bottles"
   SINGULAR_BOTTLE = "1 bottle"
 
   def verse(number)
     more_bottles_number = human_number(number)
     less_bottles_number = human_number(number - 1)
+    template = pick_template(number)
 
-    use_template(more_bottles_number, less_bottles_number)
+    use_template(template, more_bottles_number, less_bottles_number)
   end
 
   private
-  def use_template(more_bottles_number, less_bottles_number)
-    TEMPLATE % {
+  def use_template(template, more_bottles_number, less_bottles_number)
+    template % {
       more_bottles: bottle_form(more_bottles_number),
       less_bottles: bottle_form(less_bottles_number),
       taken: taken_form(more_bottles_number)
     }
+  end
+
+  def pick_template(number)
+    if number > 0
+      TEMPLATE
+    else
+      TEMPLATE_EMPTY
+    end
   end
 
   def bottle_form(number)
