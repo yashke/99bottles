afd40a3d3bc825bd21ad1f1024e537412bc50ec6
Jan Filipowski <jachuf@gmail.com>
2016-10-15 21:56:52 +0200

    6th test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,13 +1,17 @@
-class Bottles
+class Verse
   TEMPLATE = "%{more_bottles} of beer on the wall, %{more_bottles} of beer.\nTake %{taken} down and pass it around, %{less_bottles} of beer on the wall.\n"
   TEMPLATE_EMPTY = "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
   PLURAL_BOTTLE = "%s bottles"
   SINGULAR_BOTTLE = "1 bottle"
 
-  def verse(number)
-    more_bottles_number = human_number(number)
-    less_bottles_number = human_number(number - 1)
-    template = pick_template(number)
+  def initialize(number)
+    @number = number
+  end
+
+  def to_s
+    more_bottles_number = human_number(@number)
+    less_bottles_number = human_number(@number - 1)
+    template = pick_template(@number)
 
     use_template(template, more_bottles_number, less_bottles_number)
   end
@@ -51,5 +55,24 @@ class Bottles
     else
       "#{number}"
     end
+  end
+end
+
+class Bottles
+  def verse(number)
+    Verse.new(number).to_s
+  end
+
+  def verses(upper_limit, lower_limit)
+    verses = verses_for_range(lower_limit..upper_limit)
+    verses.map(&:to_s).join("\n")
+  end
+
+  private
+  def verses_for_range(range)
+    inversed_verses = range.map do |number|
+      Verse.new(number)
+    end
+    inversed_verses.reverse
   end
 end
